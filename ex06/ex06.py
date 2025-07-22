7c52536025e108fec1b4fe962577991f16ef81b3
s65v37 <ryan20091227@gmail.com>
2025-07-22 19:26:56 +0800

    Add printing hello world to ex06.py

--- ex06/ex06.py
+++ ex06/ex06.py
@@ -20,3 +20,5 @@ end12 = "y"
 # watch end =' ' at the end.try removeing it to see what happens
 print(end1+end2+end3+end4+end5+end6+end7+end8,end=' ')
 print(end9+end10+end11+end12)
+
+print("hello world!!!")
