574ebb21871e482c3ffd57bb8469414bc6aed2c4
s65v37 <ryan20091227@gmail.com>
2025-07-29 19:15:56 +0800

    add works for ex09.py

--- ex09/ex09.py
+++ ex09/ex09.py
@@ -1,1 +1,22 @@
 print("hello world")
+day = "mon tue wed thu fri sat sun"
+month = "Jar \nFeb \nMar \nApr \nMay \nJun \nJul \nAug"
+print("Here have a day",day)
+print("here have a month",month)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+even 4 lines if we want, or 5, or 6.
+""")
+print("""jar
+feb
+mar
+apr
+may
+may
+jun
+jul
+aug
+""")
